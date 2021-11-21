1f572c77931b1738710f04e0d343dd68e0347036
Александр <k142@mail.ru>
2021-11-21 13:16:57 +0300

    project init2

--- project/hello.py
+++ project/hello.py
@@ -1,4 +1,7 @@
-from flask import Flask
+import json
+
+from flask import Flask, jsonify
+from flask import request
 
 app = Flask(__name__)
 
@@ -6,6 +9,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello World 2 !</h1>'
+
+
+@app.route('/series/', methods=['POST'])
+def new_series():
+    series = request.json
+    return '<h1>Waiting for results</h1>' +  json.dumps(series)
 
 
 if __name__ == '__main__':
